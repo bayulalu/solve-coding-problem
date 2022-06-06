40a5d6258f28604b532116c5880aee8ace0663c2
bayulalu <bayulalu3@gmail.com>
2022-06-06 13:08:16 +0000

    solusi number 2

--- index.js
+++ index.js
@@ -12,21 +12,51 @@
 
 // 2 parametes - aaray - no size limit
 // return true or false 
-const array1 = ['a', 'b', 'c', 'x'];
+
+// const array1 = ['a', 'b', 'c', 'x'];
+// const array2 = ['z', 'y', 'x'];
+
+// function containsComonItem(arr1, arr2){
+//   for(let i=0; i < arr1.length; i++){
+//     for(let j = 0; j < arr2.length; j++){
+//       if(arr1[i] === arr2[j]){
+//         console.log('true');
+//         return true;
+//       }
+//     }
+//   }
+//         console.log('false');
+
+//   return false;
+// }
+
+// containsComonItem(array1, array2);
+// O(a*b)
+
+// ==== solusion number 2 ===
+const array1 = [ 'a', 'b', 'c', 'p'];
 const array2 = ['z', 'y', 'x'];
 
-function containsComonItem(arr1, arr2){
+function containsCommonItem2(arr1, arr2){
+  // loop through first array and create create object where properties === item in the array 
+  let map = {};
   for(let i=0; i < arr1.length; i++){
-    for(let j = 0; j < arr2.length; j++){
-      if(arr1[i] === arr2[j]){
-        console.log('true');
-        return true;
-      }
+    if(!map[arr1[i]]){
+      const item = arr1[i];
+      map[item] = true;
     }
   }
-        console.log('false');
-
+  console.log(map)
+  // loop through second array and check if item in secound array exists on created object
+  for(let j=0; j < arr2.length; j++){
+    if(map[arr2[j]]){
+       console.log('true');
+      return true;
+    }
+  }
+   console.log('false'); 
   return false;
 }
 
-containsComonItem(array1, array2);
+containsCommonItem2(array1, array2);
+// O(a+b) Time Complexiity
